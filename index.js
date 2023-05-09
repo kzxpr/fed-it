315f1ea291d3999dcc3097df5f55d780d7399d14
kzxpr <kzxpr@protonmail.com>
2023-05-09 17:20:16 +0200

    Using handlebars

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3013;
+const { engine } = require("express-handlebars");
 
-app.use('/', express.static('public'))
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.use('/public', express.static('public'))
+
+app.get("/", (req, res) => {
+	res.render("start");
+});
+
+app.get("/platform", (req, res) => {
+	res.render("platform");
+});
 
 app.get('*', (req, res) => {
 	res.sendStatus(404);
